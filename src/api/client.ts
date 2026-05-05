0ac12b6dd8c25b911f01fa42b3aeccd3ce43cc7c
Sa Sodanin <sasodanin04@gmail.com>
2026-05-06 00:55:16 +0700

    add axios client with auth interceptor

---
+++ src/api/client.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const client = axios.create({
+  baseURL: 'https://dev.tovtrip.com/usersvc/api/v1',
+  headers: {
+    apikey: '037cb34d-c5ee-4169-b2fd-bec049f77ecf',
+    'x-platform': 'android',
+  },
+});
+
+// Request interceptor to add JWT token from AsyncStorage
+client.interceptors.request.use(
+  async config => {
+    try {
+      const token = await AsyncStorage.getItem('authToken');
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    } catch (error) {
+      console.error('Error reading auth token from AsyncStorage:', error);
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
+
+export default client;
